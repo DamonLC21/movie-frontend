eca16d03194a267120856451072a7b2f47fb43bc
Damon Chivers <36902693+DamonLC21@users.noreply.github.com>
2020-08-06 12:51:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 let imageID = // pick a number between 1 and 3.
 let imageURL = `http://localhost:3000/movies/${imageID}`
-
+let upvotesURL = 'http://localhost:3000/upvotes'
+let reviewsURL = 'http://localhost:3000/reviews'
